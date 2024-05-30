04b0087746967927f649f4c053e093729534fceb
ChaLeVerdier <contact@art-graphic-leverdier.com>
2024-05-30 21:26:48 +0200

    Intégrations des pages pour tous les medias (de 1440px à 320px) terminée - reste le menu hamburger à solutionner

--- themes/planty/header-custom.php
+++ themes/planty/header-custom.php
@@ -34,6 +34,8 @@
     <?php
     //mon menu mobile
      if ( has_nav_menu( 'mobile_menu' ) ) {
+      // echo '<input type="checkbox" id="menu-toggle" />';
+      // echo '<label for="menu-toggle" class="menu-icon">&#9776;</label>';
             wp_nav_menu( array(
                 'theme_location' => 'mobile_menu',
                 'menu_id'        => 'site-navigation',
@@ -42,46 +44,10 @@
                 'container_class' => 'main-nav mobile-menu',
                 'container_role' => 'navigation', // Ajouter l'attribut role="navigation"
                 'items_wrap'     => '<ul class="nav-list">%3$s</ul>',
-                'menu_class'     => 'menu',
+                // 'menu_class'     => 'menu',
                 'fallback_cb'      => false,
             ) );
         }
     ?>
 </header>
 
-<!-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav> -->
